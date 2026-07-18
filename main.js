570fa66a4f1e9bb3892693411be3306ced39a357
artem_lazariev <lazarievartem25@gmail.com>
2026-07-18 17:42:35 +0200

    test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 async function getLinks(link){
     return await fetch(link).then(res => res.text()).catch(err => console.log(err));
 }
-for (let i of document.querySelectorAll("l")){
+for (let i of document.querySelectorAll("l")) {
     await getLinks(i.innerHTML).then(async res => i.outerHTML = res);
-}////.
+}
