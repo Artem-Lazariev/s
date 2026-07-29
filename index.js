2845d7f8bc24511ad88319005d1b4f7df2e5670c
Artem-Lazariev <lazarievartem25@gmail.com>
2026-07-29 19:06:36 +0200

    Refactor change function to accept inAll parameter

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 
-//base.innerHTML = ''
-function change(obj,index = 0) {
-    let base = document.querySelectorAll('all')
+function change(obj,index = 0,inAll = 0) {
+    let base = document.querySelectorAll('all')[inAll];
     let array = []
-    for (let i of base) {
+    let i = base
 
-        i = i.querySelectorAll('var')[index]
-        if (!i || !(obj[i.innerHTML.trim()]) ) {
-            array.push(-1)
-        }else {
-            i.innerHTML = obj[i.innerHTML.trim()]
-            array.push(1)
-        }
+    i = i.querySelectorAll('var')[index]
+    if (!i || !(obj[i.innerHTML.trim()]) ) {
+        array.push(-1)
+    }else {
+        i.innerHTML = obj[i.innerHTML.trim()]
+        array.push(1)
     }
+
     return array
 }
-change({name:"Artem"},0);
-//
+change({name:"Artem"},0,0);
