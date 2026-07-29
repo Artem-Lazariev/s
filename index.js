3fb90767854bf3f2e116aba9149927851d563055
artem_lazariev <lazarievartem25@gmail.com>
2026-07-29 15:40:49 +0200

    a

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//base.innerHTML = ''
+function change(obj,index = 0) {
+    let base = document.querySelectorAll('all')
+    let array = []
+    for (let i of base) {
+
+        i = i.querySelectorAll('var')[index]
+        if (!i || !(obj[i.innerHTML.trim()]) ) {
+            array.push(-1)
+        }else {
+            i.innerHTML = obj[i.innerHTML.trim()]
+            array.push(1)
+        }
+    }
+    return array
+}
+change({name:"Artem"},0);
